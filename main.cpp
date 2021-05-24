dac6e0ccc18eed25e3d98c99d3dfed6aaa982070
RachevNikita <Prib-201_831838@volsu.ru>
2021-05-24 22:50:17 +0300

    добавил в программу вывод суммы введенных чисел

--- main.cpp
+++ main.cpp
@@ -17,6 +17,9 @@ int main()
 
     cout << "number_one: " << number_one << endl;
      cout << "number_two: " << number_two << endl;
+	
+	c=number_one+number_two;
+    cout << "Сумма двух чисел: " << c << endl;
     
     return 0;
 }
