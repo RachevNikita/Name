6ca46f20e4e2de3a8115de0668fdc9bd9e4a418f
RachevNikita <Prib-201_831838@volsu.ru>
2021-05-24 22:46:14 +0300

    Добавил в программу ввод двух целых чисел

--- main.cpp
+++ main.cpp
@@ -4,6 +4,19 @@
 
 int main()
 {
+	setlocale(LC_ALL, "Rus");
+
+    int number_one;
+    int number_two;
+    int c;
+    cout << "Введите два целых числа: ";
+
+    cin >> number_one;
+     cin >> number_two;
+
+
+    cout << "number_one: " << number_one << endl;
+     cout << "number_two: " << number_two << endl;
     
     return 0;
 }
