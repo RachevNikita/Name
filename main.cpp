a23d44ffaf59fb533d99db185928c9ba90bfd8aa
RachevNikita <Prib-201_831838@volsu.ru>
2021-05-24 23:31:06 +0300

    Изменил код.Вывод произведения и частного

--- main.cpp
+++ main.cpp
@@ -18,11 +18,11 @@ int main()
     cout << "number_one: " << number_one << endl;
      cout << "number_two: " << number_two << endl;
 	
-	c=number_one+number_two;
-    cout << "Сумма двух чисел: " << c << endl;
+	c=number_one*number_two;
+    cout << "Произведение двух чисел: " << c << endl;
 
-c=number_one-number_two;
-    cout << Разность двух чисел: " << c << endl;
+c=number_one/number_two;
+    cout << деление двух чисел: " << c << endl;
     
     return 0;
 }
